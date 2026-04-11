96c959d5e2ef97ae6cba418d1456b87d307e64dd
Kyle Pointer <kpointer@stevenswater.com>
2026-04-10 17:31:40 -0700

    Update project_wbgt.py

--- project_wbgt.py
+++ project_wbgt.py
@@ -26,11 +26,11 @@ def Buck_equation(DB):
         print("DB ERR! new DB=", d)
     return 0.61121 * np.exp((18.678-(d/234.5))*(d/(257.14+d)))
 
-# Approximates wet bulb temp from v_a, t_a, t_g, and RH:
+# Approximates wet bulb temp from v_a, t_a, t_g, RH, and pressure:
 def approximate_wet_bulb(v, a, g, h):
     # Arbitrary starting values:
-    w = dew_pt(a, h)
-    p_w = Buck_equation(w)
+    w_guess = dew_pt(a, h)
+    p_w = Buck_equation(w_guess)
     p_a = Buck_equation(a)
     r = mean_radiant_from_globe(g, v, a)
     dir = 1
@@ -67,7 +67,7 @@ def approximate_wet_bulb(v, a, g, h):
     return w_guess
 
 # Approximates wet bulb temp from v_a, t_a, t_g, RH, and pressure:
-def approximate_wet_bulb_psychro(v, a, g, h, p):
+def approximate_wet_bulb_ISO(v, a, g, h, p):
     w_psy = 0
     if v == 0:
         return psychrometric_wet_bulb(a, p, h)
@@ -148,10 +148,105 @@ def psychrometric_wet_bulb(a, p, h):
     
     return w_guess
 
+def wet_bulb_liljegren(t_aC, rh_percent, p_mBar, windspeed_mps, radiance_Wpm2):
+    # Final target value, wet bulb temperature:
+    t_wC = t_aC                         # use ambient temp as starting guess, Celsius
+
+    # input unit conversions:
+    t_aK = t_aC + 273.15                # Celsius -> Kelvin
+    t_wK = t_wC + 273.15                # Celsius -> Kelvin
+    rh = rh_percent/100.0               # percent -> ratio
+    P = p_mBar*100                      # millibar -> Pascal
+    u = windspeed_mps                   # N/A; better variable name
+    S_dir = radiance_Wpm2               # N/A; better variable name
+
+    # Constants:
+    g = 9.81                            # gravitational constant, m/s^2
+    sigma = 0.0000000567                # Stefan-Boltzmann constant, W/m^2*K^4
+    R_a = 287.05                        # ???, J/kg*K TODO
+    mu_0 = 0.00001716                   # Sutherland reference constant for air viscosity at reference temp, kg/ms
+    t_0K = 273.15                       # reference temperature for mu_0, Kelvin
+    S_K = 110.4                         # Sutherland temperature, Kelvin
+    c_p = 1005                          # specific heat of air, J/kg*K
+    P_0 = 101325                        # pressure constant?, Pascals  TODO: verify
+    D_0 = 0.0000226                     # diffusivity constant?, m^2/s  TODO: verify
+
+    # Experimental constants (experimentally-fixed variables):
+    D = 0.007                           # wick diameter, m
+    E_wick = 0.95                       # wick emissivity, ratio
+    E_atm = 0.85                        # atmospheric emissivity, ratio TODO: double-check approximation
+    alpha = 0.4                         # wick albedo, ratio
+    # theta_z = 45                        # Solar zenith angle, degrees
+    # theta_zRad = np.deg2rad(theta_z)    # Solar zenith angle, radians
+
+    # Arbitrary starting values:
+    dir = -1                            # t_aC > t_wC, so start from ambient and decrease
+    incr = 10
+    Q_net = 1
+    Q_net_prev = Q_net
+
+    i = 1000
+    while i > 0:
+        i -= 1
+        # Calculate values:
+        t_wK = t_wC + 273.15                                                # Celsius -> Kelvin
+        # t_fK = temp_fK(t_aK, t_wK)                                          # film temperature, Kelvin
+        e_sa = Buck_eq_Pa(t_aC)                                             # saturation vapor pressure at ambient temperature, Pascals
+        e_a = rh * e_sa                                                     # partial water vapor pressure at ambient temperature, Pascals
+        e_sw = Buck_eq_Pa(t_wC)                                             # saturation vapor pressure at wet bulb temperature, Pascals
+        q = 0.622 * e_a / (P-0.378*e_a)                                     # specific humidity, ratio
+        rho = P / (R_a*t_aK*(1+0.61*q))                                     # air density, kg/m^3
+        mu = mu_0 * np.float_power(t_aK/t_0K,3/2) * (t_0K+S_K)/(t_aK+S_K)   # dynamic viscosity of air (Sutherland eq.), kg/m*s
+        k = 0.0241 * np.float_power(t_aK/273.15,0.9)                        # thermal conductivity of air, W/m*k
+        D_v = D_0 * np.float_power(t_aK/273.15,1.75) * P_0/P                # water vapor diffusivity in air, m^2/s
+        Pr = (c_p*mu)/k                                                     # Prandtl number, unitless TODO: verify
+        Sc = mu/(rho*D_v)                                                   # Schmidt number (evaporation efficiency), unitless TODO: verify
+        Re = (rho*u*D)/mu                                                   # Reynolds numebr (convection strength), unitless TODO: verify
+        beta = 1/t_aK                                                       # coefficient of thermal expansion, Kelvin^-1 TODO
+        v = mu/rho                                                          # kinematic viscosity, m^2/s
+        Gr = (g*beta*abs(t_wK-t_aK)*np.float_power(D,3))/np.float_power(v,2)    # Grashof number, unitless TODO: verify
+        C = 0.48                                                            # ???, ??? TODO
+        n1 = 1/4                                                            # ???, ??? TODO
+        n2 = 3                                                              # ???, ??? TODO
+        # Nusselt convection components—Forced, Natural, Combined:
+        Nu_f = 0.3 + (0.62*np.float_power(Re,1/2)*np.float_power(Pr,1/3))/(np.float_power(1 + np.float_power(0.4/Pr,2/3),1/4)) * np.float_power(1+np.float_power(Re/282000,5/8),4/5)
+        Nu_n = C * np.float_power(Gr*Pr,n1)
+        Nu = np.float_power(np.float_power(Nu_f,n2) + np.float_power(Nu_n,n2),1/n2)
+        Nu = 2.0 + 0.6*np.float_power(Re,1/2)*np.float_power(Pr,1/3)
+        h_c = (Nu*k)/D                                          # convective heat coefficient, W/m^2*K
+        Sh = Nu * np.float_power(Sc/Pr,1/3)                     # Sherwood number, unitless
+        h_e = (Sh*D_v)/D                                        # mass transfer coefficient, m/s
+        L_v = 2501000 - 2370*t_wC                               # latent heat of vaporization at wet bulb temp, J/kg
+        # F_dir = np.sin(np.deg2rad(theta_z))                     # geometric factor from zenith angle, unitless
+        # R_short = (1-alpha)*(S_dir*F_dir)                       # shortwave radiation component, W/m^2
+        F_est = 0.5                                             # estimated all-purpose geometric zenith angle factor, unitless
+        R_short = (1-alpha)*(S_dir*F_est)                       # shortwave radiation component, W/m^2
+        R_longIn = E_atm*sigma*np.float_power(t_aK,4)           # incoming longwave radiation component, W/m^2
+        R_longOut = E_wick*sigma*np.float_power(t_wK,4)         # outgoing longwave radiation component, ?
+        
+        # Calculate fundamental heat flux components:
+        Q_evap = rho*L_v*h_e * (e_sw-e_a)/(P)                   # evaporative heat flux, W/m^2
+        Q_rad = R_short + R_longIn - R_longOut                  # radiative heat flux, W/m^2
+        Q_conv = h_c * (t_aC - t_wC)                            # convective heat flux, W/m^2
+
+        # Calculate final net heat flux:
+        Q_net_prev = Q_net
+        Q_net = Q_evap + Q_rad + Q_conv                         # total net heat flux: when Q_net ≈ 0, t_w is correct
+        # print(Q_net_prev, Q_net)
+
+        if abs(Q_net) < 0.02:
+            return t_wC
+        
+        if (Q_net_prev > 0 and Q_net < 0) or (Q_net_prev < 0 and Q_net > 0):
+            dir *= -1
+            incr /= 10
+        
+        t_wC += incr * dir                                      # new wet bulb guess
+    
+    return t_wC
 
 
-
-GENERATE_MASTER_ARRAY = True
+GENERATE_MASTER_ARRAY = False
 
 # SCRIPT START:
 tgMax = 3
@@ -181,7 +276,7 @@ if (GENERATE_MASTER_ARRAY):
                 for Hum in range(0,rhMax):
                     h = hm*Hum+hb
                     # w = approximate_wet_bulb_psychro(v, a, g, h, 1013.5)
-                    w = approximate_wet_bulb(v, a, g, h)
+                    w = approximate_wet_bulb_ISO(v, a, g, h)
                     WBGT[Tg, Ta, Va, Hum] = 0.7*w + 0.2*g + 0.1*a
                     count = count + 1
             os.system("cls")
